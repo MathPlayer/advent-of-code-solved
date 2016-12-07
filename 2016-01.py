6c8571bb727bb7276f40e99c5fb64a09aeda8573
Bogdan-Constantin POPESCU <bpopescu@bitdefender.com>
2016-12-07 15:03:29 +0200

    Solve second half of the day 1 puzzle from 2016
    Signed-off-by: Bogdan-Constantin POPESCU <bogdan.popescu0905@gmail.com>

--- 2016-01.py
+++ 2016-01.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     signs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
     index = 0
     position = [0, 0]
+    visited = []
+    visited_twice = None
     with open(sys.argv[1]) as f:
         instructions = f.readline().strip().split(", ")
         for instruction in instructions:
@@ -17,8 +19,23 @@ if __name__ == "__main__":
             else:
                 continue
             steps = int(instruction[1:])
+
+            visiting = position[:]
             position[0] += steps * signs[index][0]
             position[1] += steps * signs[index][1]
 
+            if visited_twice:
+                continue
+            for _ in xrange(steps):
+                visiting[0] += signs[index][0]
+                visiting[1] += signs[index][1]
+                if not visited or visiting not in visited:
+                    visited.append(visiting[:])
+                else:
+                    visited_twice = visiting[:]
+                    break
+
+
         print abs(position[0]) + abs(position[1])
+        print abs(visited_twice[0]) + abs(visited_twice[1])
     sys.exit(0)
