b5c608a2dbf549d5ccfb2fe41d01477b98b6a9ab
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2020-12-05 11:24:47 +0100

    Refactor day 05 of 2020
    Make solution more compact after understanding what binary means in the
    problem text.

--- 2020/05.py
+++ 2020/05.py
@@ -2,46 +2,28 @@
 
 import sys
 
+TRANSLATION = str.maketrans({'F': '0', 'B': '1', 'L': '0', 'R': '1'})
+
 
 def read(in_file):
     ''' Reads data for day 05. '''
     with open(in_file) as f:
         data = tuple(x.strip() for x in f.readlines())
-            for x in f.readlines())
     return data
 
 
 def decode(seat):
-
-    def change_up(pair):
-        ''' Keeps the up part of the pair. '''
-        return (pair[0], (pair[0] + pair[1]) // 2)
-
-    def change_down(pair):
-        ''' Keeps the down part of the pair. '''
-        return ((pair[0] + pair[1]) // 2 + 1, pair[1])
     ''' Gives back the ID of a seat. '''
-
-    row = (0, 127)
-    col = (0, 7)
-
-    for char in seat:
-        if char == 'F':
-            row = change_up(row)
-        if char == 'B':
-            row = change_down(row)
-        if char == 'L':
-            col = change_up(col)
-        if char == 'R':
-            col = change_down(col)
-    return row[0] * 8 + col[0]
+    seat = seat.translate(TRANSLATION)
+    row = int(seat[:7], 2)
+    col = int(seat[7:], 2)
+    return row * 8 + col
 
 
 def solve(in_file):
     ''' Solves day 05. '''
     data = read(in_file)
     print(in_file)
-    # Get IDs.
     ids = set(map(decode, data))
     # part 1
     print(max(ids))
@@ -50,7 +32,6 @@ def solve(in_file):
         if i not in ids:
             print(i)
             return
-    print("No seat found")  # For test one
 
 
 if __name__ == '__main__':
@@ -58,3 +39,4 @@ if __name__ == '__main__':
     solve('05.in')
 
     sys.exit(0)
+
