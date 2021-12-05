ea253f2aedc4eb5a56c9e70944ea99a825e0882b
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2021-12-05 07:17:18 +0100

    Solve day 5 of 2021

---
+++ 2021/05/part2.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+
+# filename = 'input-test.txt'
+filename = 'input.txt'
+data = []
+draw_min_x = 99999
+draw_max_x = 0
+draw_min_y = 99999
+draw_max_y = 0
+for line in open(filename, 'r').readlines():
+    c1, c2 = line.split(' -> ')
+    x1, y1 = [int(x) for x in c1.split(',')]
+    x2, y2 = [int(x) for x in c2.split(',')]
+    draw_min_x = min(draw_min_x, x1, x2)
+    draw_max_x = max(draw_max_x, x1, x2)
+    draw_min_y = min(draw_min_y, y1, y2)
+    draw_max_y = max(draw_max_y, y1, y2)
+    data.append((x1, y1, x2, y2))
+
+plane = defaultdict(int)
+
+
+def draw_plane():
+    print(plane)
+    for y in range(draw_min_y, draw_max_y + 1):
+        line = ''
+        for x in range(draw_min_x, draw_max_x + 1):
+            if plane[(x, y)] == 0:
+                line += '.'
+            else:
+                line += str(plane[(x, y)])
+        print(line)
+
+
+for (x1, y1, x2, y2) in data:
+    if y1 == y2:
+        min_x = min(x1, x2)
+        max_x = max(x1, x2)
+        for x in range(min_x, max_x + 1):
+            plane[(x, y1)] += 1
+    elif x1 == x2:
+        min_y = min(y1, y2)
+        max_y = max(y1, y2)
+        for y in range(min_y, max_y + 1):
+            plane[(x1, y)] += 1
+    else:
+        x = x1
+        y = y1
+        sign_x = 1 if x1 < x2 else -1
+        sign_y = 1 if y1 < y2 else -1
+        while x != x2 and y != y2:
+            plane[(x, y)] += 1
+            x += sign_x
+            y += sign_y
+        plane[(x2, y2)] += 1
+
+# Count points on plane with value >= 2
+count = len([v for v in plane.values() if v >= 2])
+print(f"{count=}")
